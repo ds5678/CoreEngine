791469c1e7747a80dca2193850aa7e12ef6c410c
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-05-21 01:08:47 -0400

    Push some WIP audio code

--- CoreEngine.Subsets.Audio/Program.cs
+++ CoreEngine.Subsets.Audio/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace CoreEngine.Subsets.Audio;
+﻿using OpenTK.Audio.OpenAL;
+
+namespace CoreEngine.Subsets.Audio;
 
 internal class Program
 {
 	static void Main(string[] args)
 	{
+		{
+			ALDevice device = ALC.OpenDevice(null);
+			ALContext context = ALC.CreateContext(device, (int[]?)null);
+			ALC.MakeContextCurrent(context);
+
+			ALContext currentContext = ALC.GetCurrentContext();
+			if (currentContext != IntPtr.Zero)
+			{
+				ALDevice currentDevice = ALC.GetContextsDevice(currentContext);
+				string currentDeviceName = ALC.GetString(currentDevice, AlcGetString.DefaultDeviceSpecifier);
+				Console.WriteLine($"Current Audio Device Name: {currentDeviceName}");
+			}
+			else
+			{
+				Console.WriteLine("No current context found.");
+			}
+
+			// Don't forget to clean up the context and device
+			ALC.DestroyContext(context);
+			ALC.CloseDevice(device);
+		}
 		foreach (string device in OpenTK.Audio.OpenAL.ALC.GetString(OpenTK.Audio.OpenAL.AlcGetStringList.AllDevicesSpecifier))
 		{
 			Console.WriteLine(device);
---
+++ CoreEngine.Subsets.Audio/README.md
@@ -0,0 +1,8 @@
+﻿# Audio Subset
+
+This project tests the audio capabilities of the engine.
+
+## Notes
+
+* OpenAL did not come preinstalled on my machine.
+  * I had to install it from [here](https://www.openal.org/downloads/oalinst.zip).
